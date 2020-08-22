f743a44f62803ab2eec5eaa6cc82b07c1550ca8b
Amy Franz <amy.franz@gmail.com>
2020-08-22 11:00:50 +0100

    working email

--- src/containers/ContactUs/ContactUs.js
+++ src/containers/ContactUs/ContactUs.js
@@ -1,73 +1,98 @@
 import React, { useState } from "react";
 
-const encode = (data) => {
-  return Object.keys(data)
-    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-    .join("&");
-};
-
 export default function ContactUs() {
-  let [email, setEmail] = useState("");
   let [name, setName] = useState("");
+  let [email, setEmail] = useState("");
+  let [subject, setSubject] = useState("");
   let [message, setMessage] = useState("");
-  const handleSubmit = (e) => {
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", name, email, message }),
-    })
-      .then(() => alert("Success!"))
-      .catch((error) => alert(error));
 
-    e.preventDefault();
+  const data_js = {
+    access_token: "lbn1qledczlkg8sayde0h6lm",
   };
+
+  function js_send() {
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+      if (request.readyState === 4 && request.status === 200) {
+        alert("success");
+      } else if (request.readyState === 4) {
+        alert("error");
+      }
+    };
+
+    data_js["subject"] = subject;
+    data_js["text"] = `${name} - ${email}: ${message}`;
+    const params = toParams(data_js);
+
+    request.open("POST", "https://postmail.invotes.com/send", true);
+    request.setRequestHeader(
+      "Content-type",
+      "application/x-www-form-urlencoded"
+    );
+
+    request.send(params);
+
+    return false;
+  }
+
+  function toParams(data_js) {
+    let form_data = [];
+    for (var key in data_js) {
+      form_data.push(
+        encodeURIComponent(key) + "=" + encodeURIComponent(data_js[key])
+      );
+    }
+
+    return form_data.join("&");
+  }
 
   return (
     <div className="contactUs">
       <h2 className="title">Contact Us</h2>
       <div>
-        <form onSubmit={handleSubmit}>
-          <p>
-            <label>
-              Your Name:{" "}
-              <input
-                type="text"
-                name="name"
-                value={name}
-                onChange={(e) => {
-                  setName(e.target.value);
-                }}
-              />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your Email:{" "}
-              <input
-                type="email"
-                name="email"
-                value={email}
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                }}
-              />
-            </label>
-          </p>
-          <p>
-            <label>
-              Message:{" "}
-              <textarea
-                name="message"
-                value={message}
-                onChange={(e) => {
-                  setMessage(e.target.value);
-                }}
-              />
-            </label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
+        <form
+          id="javascript_form"
+          onSubmit={(e) => {
+            e.preventDefault();
+            js_send(e);
+          }}
+        >
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            placeholder="Name"
+          />
+          <input
+            type="text"
+            name="email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            placeholder="Email"
+          />
+          <input
+            type="text"
+            name="subject"
+            value={subject}
+            onChange={(e) => {
+              setSubject(e.target.value);
+            }}
+            placeholder="Subject"
+          />
+          <textarea
+            name="text"
+            value={message}
+            onChange={(e) => {
+              setMessage(e.target.value);
+            }}
+            placeholder="Message"
+          ></textarea>
+          <input type="submit" id="js_send" value="Send" />
         </form>
         <div></div>
       </div>
